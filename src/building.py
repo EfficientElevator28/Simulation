0d2c603e923d3a70f0cf9c21aacd8c962374cc76
Daniel Nichols <dando18studios@gmail.com>
2020-01-14 11:46:08 -0500

    added Building class sketch

---
+++ src/building.py
@@ -0,0 +1,30 @@
+"""
+Author: Daniel Nichols
+building.py
+Defines a Building class, which contains elevators.
+"""
+
+
+class Building:
+    """
+    Building class.
+    """
+
+    def __init__(self, name, elevators):
+        """
+        Creates a building object.
+
+        Arguments:
+        name -- the name/id of this building.
+        elevators -- a list of elevators in this building.
+        """
+
+        self.name = name
+        self.elevators = elevators
+
+    def reset():
+        """
+        Resets every elevator in this building to its original position.
+        """
+        # TODO
+        pass
