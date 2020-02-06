a9364b0de3b88547939d5ebaf41d9f9e14ce16c6
Owen Barbour <obarbour@vols.utk.edu>
2020-02-06 11:21:33 -0500

    Added elevator physics parameters #5
    Signed-off-by: Owen Barbour <obarbour@vols.utk.edu>

--- src/building.py
+++ src/building.py
@@ -10,18 +10,21 @@ class Building:
     Building class.
     """
 
-    def __init__(self, name, elevators, n_floors):
+    def __init__(self, name, elevators, n_floors, floor_dist):
         """
         Creates a building object.
 
         Arguments:
         name -- the name/id of this building.
         elevators -- a list of elevators in this building.
+        n_floors -- number fo floors in building
+        floor_dist -- distance between floors (meters)
         """
 
         self.name = name
         self.elevators = elevators
         self.n_floors = n_floors
+        self.floor_dist = floor_dist
 
     def get_n_elevators(self):
         """
@@ -29,7 +32,7 @@ class Building:
         """
         return len(self.elevators)
 
-    def reset():
+    def reset(self):
         """
         Resets every elevator in this building to its original position.
         """
--- src/elevator.py
+++ src/elevator.py
@@ -8,18 +8,26 @@ class Elevator:
     Elevator class.
     """
 
-    def __init__(self, name, speed=0.5, avg_boarding_time=5, max_riders=10):
+    def __init__(self, name, max_velocity, max_acc, jerk=0, speed=0.5, avg_boarding_time=5, max_riders=10):
         """
         inits the elevator.
 
         Parameters:
         name -- name of this elevator
         speed -- elevator speed in floors/sec
-        avg_boarding_time -- average time spent at each floor
+        avg_boarding_time -- average time spent at each floor (factors in door closing/opening)
+        max_velocity -- maximum elevator velocity
+        max_acc -- max elevator acceleration
+        jerk -- elevator jerk (derivative of acceleration)
+        max_riders -- maximum personnel allowed on elevator
         """
         self.name = name
         self.speed = speed
         self.avg_boarding_time = avg_boarding_time
+        self.max_velocity = max_velocity
+        self.max_acc = max_acc
+        self.jerk = jerk
+        self.max_riders = max_riders
 
         self.riders = []
 
