d95c2fdb128d6cdaaeac5ea1ac7fd0f95b926864
Owen Barbour <obarbour@vols.utk.edu>
2020-02-23 16:20:10 -0500

    Added elevator state outline to elevators #2
    Signed-off-by: Owen Barbour <obarbour@vols.utk.edu>

--- src/Visualization/visualization.py
+++ src/Visualization/visualization.py
@@ -3,9 +3,11 @@ Author: Owen Barbour
 """
 
 import pyglet as pyglet
+import time
 from pyglet import gl
 from src.building import Building
 from src.elevator import Elevator
+from src.ElevatorState import ElevatorState
 
 
 class Visualization:
@@ -31,16 +33,27 @@ class Visualization:
         self.n_floors = building.n_floors
         self.n_elevators = len(building.elevators)
 
+        # Component coordinate locations
+        self.pos_elevators = []
+        self.pos_elevator_shafts = []
+        self.elev_section_min_x = 750
+        self.elev_section_min_y = 50
+        self.elev_section_max_x = 1150
+        self.elev_section_max_y = 650
+        self.dist_bt_elev_ratio = .8
+
+        # Determine elevator/shaft coordinates based on above parameters
+        self.elevator_component_positions()
+
         # Pyglet shader and function init
         pyglet.gl.glEnable(pyglet.gl.GL_BLEND)
         pyglet.gl.glBlendFunc(pyglet.gl.GL_SRC_ALPHA, pyglet.gl.GL_ONE_MINUS_SRC_ALPHA)
         gl.glClearColor(*self.background_color)
         self.on_draw = self.pyglet_window.event(self.on_draw)
         self.on_resize = self.pyglet_window.event(self.on_resize)
-
-        # Component coordinate locations
-        self.pos_elevators = []
-        self.pos_elevator_shafts = []
+        self.on_key_press = self.pyglet_window.event(self.on_key_press)
+        self.on_close = self.pyglet_window.event(self.on_close)
+        self.alive = 1
 
         '''
         self.label = pyglet.text.Label('Hello, world', font_name='Times New Roman', font_size=36,
@@ -54,18 +67,20 @@ class Visualization:
 
     # @pyglet_window.event
     def on_draw(self):
+        print("Running on_draw")
         self.pyglet_window.clear()
 
         # self.label.draw()
         # self.quad.draw(pyglet.gl.GL_QUADS)
         # self.quad2.draw(pyglet.gl.GL_QUADS)
-        self.elevator_component_positions(750, 50, 1150, 650, .8)
-        elevator_floors_batch = self.draw_elevator_floors(750, 50, 1150, 650)
+        elevator_floors_batch = self.draw_elevator_floors()
         elevator_shafts_batch = self.draw_elevator_shafts()
         elevators_batch = self.draw_elevators()
         elevator_floors_batch.draw()
         elevator_shafts_batch.draw()
         elevators_batch.draw()
+
+        self.pyglet_window.flip()
 
     # Default implementation (not used; resizing currently turned off)
     def on_resize(self, width, height):
@@ -75,13 +90,25 @@ class Visualization:
         pyglet.gl.glOrtho(0, width, 0, height, -1, 1)
         pyglet.gl.glMatrixMode(gl.GL_MODELVIEW)
 
+    def on_key_press(self, symbol, modifiers):
+        if symbol == pyglet.window.key.ESCAPE:  # [ESC]
+            self.alive = 0
+
+    def on_close(self):
+        self.alive = 0
+
     @staticmethod
     def create_quad_vertex_list(x, y, width, height):
         return x, y, x + width, y, x + width, y + height, x, y + height
 
-    def elevator_component_positions(self, min_x, min_y, max_x, max_y, dist_bt_elev_ratio):
+    def elevator_component_positions(self):
+        min_x = self.elev_section_min_x
+        min_y = self.elev_section_min_y
+        max_x = self.elev_section_max_x
+        max_y = self.elev_section_max_y
+
         x_range = max_x - min_x
-        x_elev_width = x_range / (self.n_elevators + ((self.n_elevators - 1) * dist_bt_elev_ratio))
+        x_elev_width = x_range / (self.n_elevators + ((self.n_elevators - 1) * self.dist_bt_elev_ratio))
         x_elev_dist_bt = (x_range - (x_elev_width * self.n_elevators)) / (self.n_elevators - 1)
 
         y_range = max_y - min_y
@@ -96,15 +123,15 @@ class Visualization:
             self.pos_elevator_shafts.append((min_elev_x, min_y, max_elev_x, max_y))
             self.pos_elevators.append((min_elev_x, min_y, max_elev_x, min_y + elev_height_px))
 
-    def draw_elevator_floors(self, min_x, min_y, max_x, max_y):
+    def draw_elevator_floors(self):
         batch = pyglet.graphics.Batch()
-        y_range = max_y - min_y
+        y_range = self.elev_section_max_y - self.elev_section_min_y
         y_floor_dist = y_range / self.n_floors
         for i in range(0, self.n_floors):
-            floor_y = min_y + i * y_floor_dist
+            floor_y = self.elev_section_min_y + i * y_floor_dist
             # self.canvas.create_line(min_x, floor_y, max_x, floor_y)
-            batch.add(2, pyglet.gl.GL_LINES, None, ('v2f', (min_x, floor_y, max_x, floor_y)),
-                      ('c3B', (0, 0, 0, 0, 0, 0)))
+            batch.add(2, pyglet.gl.GL_LINES, None, ('v2f', (self.elev_section_min_x, floor_y, self.elev_section_max_x,
+                                                            floor_y)), ('c3B', (0, 0, 0, 0, 0, 0)))
         return batch
 
     def draw_elevator_shafts(self):
@@ -128,42 +155,103 @@ class Visualization:
 
     def draw_elevators(self):
         batch = pyglet.graphics.Batch()
-        border_size = 2
+        border_size = 3
+
         for i in range(0, len(self.pos_elevators)):
-            min_x = self.pos_elevators[i][0]
-            min_y = self.pos_elevators[i][1]
-            max_x = self.pos_elevators[i][2]
-            max_y = self.pos_elevators[i][3]
+            y_range = self.elev_section_max_y - self.elev_section_min_y
+            shaft_height_meters = self.n_floors * self.building.floor_dist
+            elev_y_offset_px = y_range / shaft_height_meters * self.building.elevators[i].position
+
+            elev_min_x = self.pos_elevators[i][0]
+            elev_min_y = self.pos_elevators[i][1] + elev_y_offset_px
+            elev_max_x = self.pos_elevators[i][2]
+            elev_max_y = self.pos_elevators[i][3] + elev_y_offset_px
             if i % 2 == 0:
                 batch.add(4, pyglet.gl.GL_QUADS, None,
-                          ('v2f', (min_x, min_y, max_x, min_y, max_x, max_y, min_x, max_y)),
-                          ('c4B', self.color_elevator_even))
+                          ('v2f', (elev_min_x, elev_min_y, elev_max_x, elev_min_y, elev_max_x, elev_max_y, elev_min_x,
+                                   elev_max_y)), ('c4B', self.color_elevator_even))
                 '''batch.add(4, pyglet.gl.GL_QUADS, None,
                           ('v2f', (min_x+border_size, min_y+border_size, max_x-border_size, min_y+border_size, 
                           max_x-border_size, max_y-border_size, min_x+border_size, max_y-border_size)),
                           ('c4B', self.color_elevator_even_2))'''
             else:
                 batch.add(4, pyglet.gl.GL_QUADS, None,
-                          ('v2f', (min_x, min_y, max_x, min_y, max_x, max_y, min_x, max_y)),
-                          ('c4B', self.color_elevator_odd))
+                          ('v2f', (elev_min_x, elev_min_y, elev_max_x, elev_min_y, elev_max_x, elev_max_y, elev_min_x,
+                                   elev_max_y)), ('c4B', self.color_elevator_odd))
+            self.draw_elevator_passengers(batch, self.building.elevators[i], elev_min_x, elev_min_y, elev_max_x,
+                                          elev_max_y)
+            self.draw_elevator_outline(batch, self.building.elevators[i], border_size, elev_min_x, elev_min_y,
+                                       elev_max_x, elev_max_y)
         return batch
 
     # Displays the number of passengers in an elevator on the center of the elevator
-    def draw_elevator_passengers(self):
-        pass
+    @staticmethod
+    def draw_elevator_passengers(batch, elevator, elev_min_x, elev_min_y, elev_max_x, elev_max_y):
+        pyglet.text.Label(str(len(elevator.riders)), font_name='Times New Roman', font_size=12,
+                          x=((elev_min_x + elev_max_x) / 2), y=((elev_min_y + elev_max_y) / 2),
+                          anchor_x='center', anchor_y='center', batch=batch)
 
     # Elevator outline is non-transparent green/red indicating UP and DOWN ElevatorState
     # No outline for NO_ACTION or LOADING_UNLOADING ElevatorState
-    def draw_elevator_outline(self):
-        pass
+    # Will have to use shaders, or draw 1-4 polygons
+    def draw_elevator_outline(self, batch, elevator, border_size, elev_min_x, elev_min_y, elev_max_x, elev_max_y):
+        rect_bottom = (elev_min_x, elev_min_y, elev_max_x, elev_min_y, elev_max_x, elev_min_y + border_size,
+                       elev_min_x, elev_min_y + border_size)
+        rect_right = (elev_max_x - border_size, elev_min_y, elev_max_x, elev_min_y, elev_max_x, elev_max_y,
+                      elev_max_x - border_size, elev_max_y)
+        rect_top = (elev_min_x, elev_max_y - border_size, elev_max_x, elev_max_y - border_size, elev_max_x, elev_max_y,
+                    elev_min_x, elev_max_y)
+        rect_left = (elev_min_x, elev_min_y, elev_min_x + border_size, elev_min_y, elev_min_x + border_size, elev_max_y,
+                     elev_min_x, elev_max_y)
+        color_green = (62, 148, 72) * 4  # 0, 230, 0
+        color_red = (148, 62, 76) * 4  # 255, 51, 0
+        color_yellow = (148, 148, 62) * 4  # 255, 255, 0
+        color_gray = (128, 128, 128) * 4
+
+        if elevator.state == ElevatorState.NO_ACTION:
+            self.add_rect_solid_outline(batch, rect_bottom, rect_right, rect_top, rect_left, color_gray)
+        elif elevator.state == ElevatorState.LOADING_UNLOADING:
+            self.add_rect_solid_outline(batch, rect_bottom, rect_right, rect_top, rect_left, color_yellow)
+        elif elevator.state == ElevatorState.UP:
+            self.add_rect_solid_outline(batch, rect_bottom, rect_right, rect_top, rect_left, color_green)
+        elif elevator.state == ElevatorState.DOWN:
+            self.add_rect_solid_outline(batch, rect_bottom, rect_right, rect_top, rect_left, color_red)
+
+    @staticmethod
+    def add_rect_solid_outline(batch, rect_bottom, rect_right, rect_top, rect_left, color_rgb):
+        batch.add(4, pyglet.gl.GL_QUADS, None, ('v2f', rect_bottom), ('c3B', color_rgb))
+        batch.add(4, pyglet.gl.GL_QUADS, None, ('v2f', rect_right), ('c3B', color_rgb))
+        batch.add(4, pyglet.gl.GL_QUADS, None, ('v2f', rect_top), ('c3B', color_rgb))
+        batch.add(4, pyglet.gl.GL_QUADS, None, ('v2f', rect_left), ('c3B', color_rgb))
 
 
-def main():
+def visualization_test_main():
     elevators = [Elevator(1), Elevator(2), Elevator(3), Elevator(4)]
     building = Building(name=1, elevators=elevators, n_floors=10)
     vis = Visualization(building)
-    pyglet.app.run()
+
+    # Option 1: pyglet.app.run()
+    # Option 2: event_loop = pyglet.app.EventLoop()
+    # event_loop.run()
+    # Option 3: custom event loop which dispatches an on_draw event, which updates the screen to the current state
+    vis.pyglet_window.dispatch_event("on_draw")
+    while vis.alive == 1:
+        print("Running loop")
+        time.sleep(7)
+
+        # These three lines are needed
+        pyglet.clock.tick()
+        vis.pyglet_window.dispatch_events()
+        vis.pyglet_window.dispatch_event("on_draw")
+
+        building.elevators[0].position += 3
+        building.elevators[2].position += 2
+        building.elevators[0].state = ElevatorState.LOADING_UNLOADING
+        building.elevators[1].state = ElevatorState.UP
+        building.elevators[2].state = ElevatorState.DOWN
+        building.elevators[3].state = ElevatorState.NO_ACTION
+        # vis.pyglet_window.flip()
 
 
 if __name__ == "__main__":
-    main()
+    visualization_test_main()
