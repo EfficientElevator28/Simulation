735da71ee6bb3c200ddfefdc516742fc8db94396
Daniel Nichols <dando18studios@gmail.com>
2020-02-25 09:59:51 -0500

    small adjustments to step_func

--- src/simulator.py
+++ src/simulator.py
@@ -2,24 +2,37 @@
 author: Daniel Nichols
 """
 
+def default_step_func(cur_building):
+
+    for e in cur_building.elevators:
+        # step the current elevator e
+        pass
+
+    # now adjust all the people
+    # TODO get the people parameter form elevator
+
+    return True
 
 class Simulator:
     """
     Simulator class.
     """
 
-    def __init__(self, name, step_func):
+    def __init__(self, name, step_func=default_step_func):
         """
         initialize the Simulator.
         """
         self.name = name
         self.step_func = step_func
 
+    def init_building(self, building):
+        self.building = building
+
     def step(self):
         """
         step the Simulator forward
         """
-        return self.step_func()
+        return self.step_func(self.buiding)
 
     def run(self, timestep=1, new_thread=False):
         """
